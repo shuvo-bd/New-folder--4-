d21bac68e78531c771166da074d25a508f028cb6
shuvo-bd <shuvokarmakar1234@gmail.com>
2026-09-02 23:09:47 +0600

    Add temperature conversion formulas

--- Assignment-2.dart
+++ Assignment-2.dart
@@ -5,3 +5,12 @@ void main() {
   print("       SMART TEMPERATURE CONVERTER");
   print("====================================");
 }
+double result = 0.0;
+    
+    // Formulas:
+    // result = (temp * 9 / 5) + 32;               // Celsius → Fahrenheit
+    // result = (temp - 32) * 5 / 9;               // Fahrenheit → Celsius
+    // result = temp + 273.15;                     // Celsius → Kelvin
+    // result = temp - 273.15;                     // Kelvin → Celsius
+    // result = (temp - 32) * 5 / 9 + 273.15;      // Fahrenheit → Kelvin
+    // result = (temp - 273.15) * 9 / 5 + 32;      // Kelvin → Fahrenheit
